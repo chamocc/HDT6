740bc77c93d58d86f462a8c9f9a356ffd789f086
danGeren <ger13158@uvg.edu.gt>
2014-08-29 08:12:10 -0600

    factory

--- HDT6/src/SetFactory.java
+++ HDT6/src/SetFactory.java
@@ -1,3 +1,9 @@
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.TreeSet;
+import java.util.LinkedHashSet;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,6 +15,17 @@
  * @Edgar Chamo 13083
  * @Daniel Gerendas 13158
  */
-public class SetFactory {
-    
+public class SetFactory<E> {
+    public Set getInstance(int num){
+        switch (num){
+            case 0:
+                return new HashSet<E>();
+            case 1: 
+                return new TreeSet<E>();
+            case 2:
+                return new LinkedHashSet<E>();
+        }
+        //Nunca llega aqui
+        return new HashSet();
+    }
 }
